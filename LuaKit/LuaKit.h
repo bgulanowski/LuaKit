22ddfe33e6e0b94e054dfbd8e70451ba131e1991
Brent Gulanowski <bgulanowski@gmail.com>
2012-08-13 07:43:36 -0600

    Initial Commit

---
+++ LuaKit/LuaKit.h
@@ -0,0 +1,13 @@
+//
+//  LuaKit.h
+//  LuaKit
+//
+//  Created by Brent Gulanowski on 12-08-13.
+//  Copyright (c) 2012 Lichen Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LuaKit : NSObject
+
+@end
---
+++ LuaKit/LuaKit.m
@@ -0,0 +1,13 @@
+//
+//  LuaKit.m
+//  LuaKit
+//
+//  Created by Brent Gulanowski on 12-08-13.
+//  Copyright (c) 2012 Lichen Labs. All rights reserved.
+//
+
+#import "LuaKit.h"
+
+@implementation LuaKit
+
+@end
